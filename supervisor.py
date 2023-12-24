04c61d88371e872da772f78a4c88b354f688f639
greatpayro <realpayrovi@gmail.com>
2023-12-24 09:55:38 +0330

    supervisor class defined

---
+++ supervisor.py
@@ -0,0 +1,5 @@
+class SuperVisor():
+    def __init__(self, username, password, phonenumber):
+        self.username =username,
+        self.password = password,
+        self.phonenumber = phonenumber
